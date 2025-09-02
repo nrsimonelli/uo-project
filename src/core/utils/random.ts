43350740d70104d2c35c89084154fb5ac002b9a7
nrsimonelli <nrsimonelli@gmail.com>
2025-09-02 16:52:57 -0500

    rng

---
+++ src/core/utils/random.ts
@@ -0,0 +1,104 @@
+export type RandomNumberGeneratorType = {
+  seed: Array<string>
+  random: () => number
+}
+
+export const Rng = (
+  seed: string,
+  ...extraSeed: Array<string>
+): RandomNumberGeneratorType => {
+  const args = [seed, ...extraSeed]
+
+  const mash = Mash()
+  let s0 = mash(' ')
+  let s1 = mash(' ')
+  let s2 = mash(' ')
+  let c = 1
+
+  for (let i = 0; i < args.length; ++i) {
+    const arg = args[i]
+    if (arg !== undefined) {
+      s0 -= mash(arg)
+      if (s0 < 0) {
+        s0 += 1
+      }
+      s1 -= mash(arg)
+      if (s1 < 0) {
+        s1 += 1
+      }
+      s2 -= mash(arg)
+      if (s2 < 0) {
+        s2 += 1
+      }
+    }
+  }
+
+  return {
+    seed: args,
+    random: () => {
+      const t = 2091639 * s0 + c * 2.3283064365386963e-10 // 2^-32
+      s0 = s1
+      s1 = s2
+      return (s2 = t - (c = t | 0))
+    },
+  }
+}
+
+export const shuffleInPlace = <T>(
+  r: RandomNumberGeneratorType,
+  a: Array<T>
+): void => {
+  for (let i = a.length - 1; i > 0; --i) {
+    const j = Math.floor(r.random() * i)
+    const tmp = a[i]!
+    a[i] = a[j]!
+    a[j] = tmp
+  }
+}
+
+export const select = <T>(r: RandomNumberGeneratorType, a: Array<T>): T =>
+  a[uint32(r) % a.length]!
+export const selectIndex = <T>(
+  r: RandomNumberGeneratorType,
+  a: Array<T>
+): number => uint32(r) % a.length
+
+export const str = (() => {
+  const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz '
+  return (r: RandomNumberGeneratorType, min: number, max: number): string => {
+    let result = ''
+    const l = range(r, min, max)
+    for (let i = 0; i < l; ++i) {
+      result += charset.charAt(Math.floor(r.random() * charset.length))
+    }
+    return result
+  }
+})()
+
+export const range = (
+  r: RandomNumberGeneratorType,
+  min: number,
+  max: number
+): number => min + r.random() * (max - min)
+export const uint32 = (r: RandomNumberGeneratorType): number =>
+  r.random() * 0x100000000 // 2^32
+export const fract53 = (r: RandomNumberGeneratorType): number =>
+  r.random() + ((r.random() * 0x200000) | 0) * 1.1102230246251565e-16 // 2^-53
+
+const Mash = (): ((data: string) => number) => {
+  let n = 0xefc8249d
+
+  return (data: string): number => {
+    for (let i = 0; i < data.length; i++) {
+      n += data.charCodeAt(i)
+      let h = 0.02519603282416938 * n
+      n = h >>> 0
+      h -= n
+      h *= n
+      n = h >>> 0
+      h -= n
+      n += h * 0x100000000 // 2^32
+    }
+    return (n >>> 0) * 2.3283064365386963e-10 // 2^-32
+  }
+}
