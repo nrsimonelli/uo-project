ec9b28e6ef8a0ec26b9ea2d761b3e5a75ed7fa1b
nrsimonelli <nrsimonelli@gmail.com>
2025-09-17 13:21:22 -0500

    placeholder sprites

---
+++ src/data/units/sprites.ts
@@ -0,0 +1,137 @@
+import type { AllClassType } from '@/types/base-stats'
+// import each sprite from src/assets/sprites
+import Arbalist from '@/assets/sprites/Arbalist.png'
+import Bishop from '@/assets/sprites/Bishop.png'
+import Breaker from '@/assets/sprites/Breaker.png'
+import Cleric from '@/assets/sprites/Cleric.png'
+import Crusader from '@/assets/sprites/Crusader.png'
+import DarkKnight from '@/assets/sprites/Dark Knight.png'
+import DarkMarquessAxe from '@/assets/sprites/Dark Marquess (Axe).png'
+import DarkMarquessLance from '@/assets/sprites/Dark Marquess (Lance).png'
+import DarkMarquessStaff from '@/assets/sprites/Dark Marquess (Staff).png'
+import DarkMarquessSword from '@/assets/sprites/Dark Marquess (Sword).png'
+import DoomKnight from '@/assets/sprites/Doom Knight.png'
+import Dreadnought from '@/assets/sprites/Dreadnought.png'
+import Druid from '@/assets/sprites/Druid.png'
+import ElvenArcher from '@/assets/sprites/Elven Archer.png'
+import ElvenAugur from '@/assets/sprites/Elven Augur.png'
+import ElvenFencer from '@/assets/sprites/Elven Fencer.png'
+import ElvenSibyl from '@/assets/sprites/Elven Sibyl.png'
+import Featherbow from '@/assets/sprites/Featherbow.png'
+import Feathershield from '@/assets/sprites/Feathershield.png'
+import Featherstaff from '@/assets/sprites/Featherstaff.png'
+import Feathersword from '@/assets/sprites/Feathersword.png'
+import Fighter from '@/assets/sprites/Fighter.png'
+import Gladiator from '@/assets/sprites/Gladiator.png'
+import GreatKnight from '@/assets/sprites/Great Knight.png'
+import GryphonKnight from '@/assets/sprites/Gryphon Knight.png'
+import GryphonMaster from '@/assets/sprites/Gryphon Master.png'
+import HighLord from '@/assets/sprites/High Lord.png'
+import HighPriestess from '@/assets/sprites/High Priestess.png'
+import Hoplite from '@/assets/sprites/Hoplite.png'
+import Housecarl from '@/assets/sprites/Housecarl.png'
+import Hunter from '@/assets/sprites/Hunter.png'
+import Knight from '@/assets/sprites/Knight.png'
+import Landsknecht from '@/assets/sprites/Landsknecht.png'
+import Legionnaire from '@/assets/sprites/Legionnaire.png'
+import Lord from '@/assets/sprites/Lord.png'
+import Paladin from '@/assets/sprites/Paladin.png'
+import Priestess from '@/assets/sprites/Priestess.png'
+import Prince from '@/assets/sprites/Prince.png'
+import RadiantKnight from '@/assets/sprites/Radiant Knight.png'
+import Rogue from '@/assets/sprites/Rogue.png'
+import SaintedKnight from '@/assets/sprites/Sainted Knight.png'
+import Sellsword from '@/assets/sprites/Sellsword.png'
+import Sergeant from '@/assets/sprites/Sergeant.png'
+import Shaman from '@/assets/sprites/Shaman.png'
+import Shieldshooter from '@/assets/sprites/Shieldshooter.png'
+import Sniper from '@/assets/sprites/Sniper.png'
+import SnowRanger from '@/assets/sprites/Snow Ranger.png'
+import Soldier from '@/assets/sprites/Soldier.png'
+import Sorceress from '@/assets/sprites/Sorceress.png'
+import Swordfighter from '@/assets/sprites/Swordfighter.png'
+import Swordmaster from '@/assets/sprites/Swordmaster.png'
+import Thief from '@/assets/sprites/Thief.png'
+import Valkyria from '@/assets/sprites/Valkyria.png'
+import Vanguard from '@/assets/sprites/Vanguard.png'
+import Viking from '@/assets/sprites/Viking.png'
+import Warlock from '@/assets/sprites/Warlock.png'
+import Warrior from '@/assets/sprites/Warrior.png'
+import Werebear from '@/assets/sprites/Werebear.png'
+import Werefox from '@/assets/sprites/Werefox.png'
+import Werelion from '@/assets/sprites/Werelion.png'
+import Wereowl from '@/assets/sprites/Wereowl.png'
+import Werewolf from '@/assets/sprites/Werewolf.png'
+import Witch from '@/assets/sprites/Witch.png'
+import Wizard from '@/assets/sprites/Wizard.png'
+import WyvernKnight from '@/assets/sprites/Wyvern Knight.png'
+import WyvernMaster from '@/assets/sprites/Wyvern Master.png'
+
+export const SPRITES: Record<AllClassType, string> = {
+  Arbalist,
+  Bishop,
+  Breaker,
+  Cleric,
+  Crusader,
+  'Dark Knight': DarkKnight,
+  'Dark Marquess (Axe)': DarkMarquessAxe,
+  'Dark Marquess (Lance)': DarkMarquessLance,
+  'Dark Marquess (Staff)': DarkMarquessStaff,
+  'Dark Marquess (Sword)': DarkMarquessSword,
+  'Doom Knight': DoomKnight,
+  Dreadnought,
+  Druid,
+  'Elven Archer': ElvenArcher,
+  'Elven Augur': ElvenAugur,
+  'Elven Fencer': ElvenFencer,
+  'Elven Sibyl': ElvenSibyl,
+  Featherbow,
+  Feathershield,
+  Featherstaff,
+  Feathersword,
+  Fighter,
+  Gladiator,
+  'Great Knight': GreatKnight,
+  'Gryphon Knight': GryphonKnight,
+  'Gryphon Master': GryphonMaster,
+  'High Lord': HighLord,
+  'High Priestess': HighPriestess,
+  Hoplite,
+  Housecarl,
+  Hunter,
+  Knight,
+  Landsknecht,
+  Legionnaire,
+  Lord,
+  Paladin,
+  Priestess,
+  Prince,
+  'Radiant Knight': RadiantKnight,
+  Rogue,
+  'Sainted Knight': SaintedKnight,
+  Sellsword,
+  Sergeant,
+  Shaman,
+  Shieldshooter,
+  Sniper,
+  'Snow Ranger': SnowRanger,
+  Soldier,
+  Sorceress,
+  Swordfighter,
+  Swordmaster,
+  Thief,
+  Valkyria,
+  Vanguard,
+  Viking,
+  Warlock,
+  Warrior,
+  Werebear,
+  Werefox,
+  Werelion,
+  Wereowl,
+  Werewolf,
+  Witch,
+  Wizard,
+  'Wyvern Knight': WyvernKnight,
+  'Wyvern Master': WyvernMaster,
+}
