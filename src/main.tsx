096cddaea3521b269946183dbb34eedfc49bd0de
nrsimonelli <nrsimonelli@gmail.com>
2026-01-06 13:54:08 -0600

    single tooltip provider with delay

--- src/main.tsx
+++ src/main.tsx
@@ -7,18 +7,21 @@ import { ErrorBoundary } from '@/components/error-boundary'
 import { TeamProvider } from '@/components/team-builder/team-context'
 import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from '@/components/ui/sonner'
+import { TooltipProvider } from '@/components/ui/tooltip'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <ErrorBoundary>
-          <TeamProvider>
-            <App />
-            <Toaster />
-          </TeamProvider>
-        </ErrorBoundary>
+        <TooltipProvider delay={300}>
+          <ErrorBoundary>
+            <TeamProvider>
+              <App />
+              <Toaster />
+            </TeamProvider>
+          </ErrorBoundary>
+        </TooltipProvider>
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
